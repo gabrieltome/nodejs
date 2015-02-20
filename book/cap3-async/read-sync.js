7e5639be64a90f505c953c72bca9abe11b83cc14
Gabriel Tomé <gabrieltsistemas@hotmail.com>
2015-02-19 23:02:55 -0200

    fix bugs

--- book/cap3-async/read-sync.js
+++ book/cap3-async/read-sync.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var readSync = function(file){
 	console.log("Sync - Reading...");
 	var start = new Date().getTime();
-	fs.fs.readFileSync(file);
+	fs.readFileSync(file);
 	var end = new Date().getTime();
 	console.log("Sync block: "+ ( end - start ) +"ms");
 };
